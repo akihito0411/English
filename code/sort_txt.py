15b37f2c4f9fb2e62e64dbb052b03a527142a52f
akihito0411 <136040577+akihito0411@users.noreply.github.com>
2024-05-23 16:34:22 +0900

    Add files via upload

---
+++ code/sort_txt.py
@@ -0,0 +1,55 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+#nltkのデータセットをダウンロード(初回のみ)
+nltk.download('punkt')
+
+#ファイルの読み込み
+def read_file(file_path):
+    with open(file_path,'r',encoding = 'latin-1') as file:
+        return file.read()
+
+ #ソート関数   
+def sort_context(text):
+    words = word_tokenize(text)#トークン毎に格納
+    count = {}
+
+    for word in words:#要らない単語をフィルタリング
+        if word == "," or word == "." or word == "?" or word == "“" or word == "”" or word == "’" or word == "!" or word == ";" or word == "=" or word == ")" or word == "(":
+            count[word] = 0
+        else:
+            count[word] = count.get(word,0)+1
+
+    #頻出順にソート
+    d = [(v, k) for k, v in count.items()]
+    d.sort()
+    d.reverse()
+
+    return d[:20]
+
+def write_file(token,author_box):
+    start = 0
+    end = 20
+    with open("author_output.txt", 'w', encoding='latin-1') as file:
+        for i in author_box:
+            file.write(i)
+            file.write("\n")
+            #print(i)
+            #print("\n")
+            for count,word in token[start:end]: #20個並べる token[:20]
+                file.write(f"{word}:{count}\n")
+                #print(f"{word}:{count}\n")
+            start = start + 20
+            end = end + 20
+
+def main():
+    author_box = ["AnwarKhoirul_20.txt", "AokiToshiaki_4.txt","AsanoFumihiko_1.txt","ChenJiageng_6.txt","CheongKaiYuen_1.txt","DangJiannwu_5.txt","DefagoXavier_1.txt","IkedaKokolo_2.txt","InoguchiYasushi_1.txt","MatsumotoTadashi_19.txt","WirelessComm_unknown.txt"]
+    all_token = []
+
+    for i in range(11):
+        text = read_file(author_box[i])
+        all_token.extend(sort_context(text))
+    write_file(all_token,author_box)
+
+if (__name__ == "__main__"):
+    main()
